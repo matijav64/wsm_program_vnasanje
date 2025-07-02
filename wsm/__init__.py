9273ea25dbe2099c10c292f2e3e16961e74ea8c1
matijav64 <matijavidmar98@gmail.com>
2025-07-02 09:05:33 +0200

    Add package docstring and tweak tests support

--- wsm/__init__.py
+++ wsm/__init__.py
@@ -1,1 +1,5 @@
+"""White-label Supplier Manager (WSM).
 
+Tools for parsing invoices, linking items to WSM codes and providing
+command-line or graphical interfaces for processing supplier invoices.
+"""
