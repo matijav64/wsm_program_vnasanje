b453aea0d997788a6b16931f53d82bfafc55b099
matijav64 <matijavidmar98@gmail.com>
2025-08-06 18:47:00 +0200

    Prioritize VAT ID over GLN for supplier code extraction in eslog.py and gui.py, with regression tests

--- tests/test_supplier_extraction.py
+++ tests/test_supplier_extraction.py
@@ -1,23 +1,68 @@
-from lxml import etree as LET
+# flake8: noqa
+import pytest
+from lxml import etree
 
 from wsm.parsing.eslog import get_supplier_info
 
 
-def test_get_supplier_info_prefers_vat():
-    xml = """
-    <Invoice xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
-             xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
-      <cac:AccountingSupplierParty>
-        <cac:Party>
-          <cac:PartyIdentification>
-            <cbc:ID schemeID="0088">1234567890005</cbc:ID>
-          </cac:PartyIdentification>
-          <cac:PartyTaxScheme>
-            <cbc:CompanyID schemeID="VA">SI69092958</cbc:CompanyID>
-          </cac:PartyTaxScheme>
-        </cac:Party>
-      </cac:AccountingSupplierParty>
-    </Invoice>
+@pytest.fixture
+def sample_xml():
+    xml_str = """
+<Invoice xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
+         xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
+  <cac:AccountingSupplierParty>
+    <cac:Party>
+      <cac:PartyTaxScheme>
+        <cbc:CompanyID>SI69092958</cbc:CompanyID>
+      </cac:PartyTaxScheme>
+      <cac:PartyIdentification>
+        <cbc:ID schemeID="0088">3830045969997</cbc:ID>
+      </cac:PartyIdentification>
+    </cac:Party>
+  </cac:AccountingSupplierParty>
+</Invoice>
     """
-    tree = LET.ElementTree(LET.fromstring(xml))
-    assert get_supplier_info(tree) == "SI69092958"
+    return etree.fromstring(xml_str.encode())
+
+
+def test_get_supplier_info_prioritizes_vat(sample_xml):
+    code = get_supplier_info(sample_xml)
+    assert code == "SI69092958"
+
+
+def test_get_supplier_info_fallback_to_gln():
+    xml_str = """
+<Invoice xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
+         xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
+  <cac:AccountingSupplierParty>
+    <cac:Party>
+      <cac:PartyIdentification>
+        <cbc:ID schemeID="0088">3830045969997</cbc:ID>
+      </cac:PartyIdentification>
+    </cac:Party>
+  </cac:AccountingSupplierParty>
+</Invoice>
+    """
+    tree = etree.fromstring(xml_str.encode())
+    code = get_supplier_info(tree)
+    assert code == "3830045969997"
+
+
+def test_get_supplier_info_custom_va_tag():
+    xml_str = """
+<Invoice>
+  <VA>SI69092958</VA>
+</Invoice>
+    """
+    tree = etree.fromstring(xml_str.encode())
+    code = get_supplier_info(tree)
+    assert code == "SI69092958"
+
+
+def test_get_supplier_info_unknown():
+    xml_str = """
+<Invoice />
+    """
+    tree = etree.fromstring(xml_str.encode())
+    code = get_supplier_info(tree)
+    assert code == "Unknown"
